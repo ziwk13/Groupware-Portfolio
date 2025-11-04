fa240c8d91225a4fbff67d6ff8b25a220cf532f7
ziwk13 <ziwk13@hanmail.com>
2025-11-04 17:16:47 +0900

    EmployeeIcon.jsx - 시스템 메시지 아이콘 수정

--- src/assets/icons/EmployeeIcon.jsx
+++ src/assets/icons/EmployeeIcon.jsx
@@ -2,22 +2,21 @@ import PropTypes from 'prop-types';
 
 const EmployeeIcon = ({ size = 24, ...props }) => {
   return (
-    <svg 
-    xmlns="http://www.w3.org/2000/svg" 
-    width={size} 
-    height={size}  
-    viewBox="0 0 24 24" 
-    fill="currentColor" 
-    className="icon icon-tabler icons-tabler-filled icon-tabler-mickey"
-    {...props}
-    >
-      <path 
-      stroke="none" 
-      d="M0 0h24v24H0z" 
-      fill="none"/>
-      <path
-      d="M18.501 2a4.5 4.5 0 0 1 .878 8.913a8 8 0 1 1 -15.374 3.372l-.005 -.285l.005 -.285a7.991 7.991 0 0 1 .615 -2.803a4.5 4.5 0 0 1 -3.187 -6.348a4.505 4.505 0 0 1 3.596 -2.539l.225 -.018l.281 -.007l.244 .009a4.5 4.5 0 0 1 4.215 4.247a8.001 8.001 0 0 1 4.013 0a4.5 4.5 0 0 1 4.493 -4.256z" />
-      </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-user">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+      <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+    </svg>
   );
 };
 
