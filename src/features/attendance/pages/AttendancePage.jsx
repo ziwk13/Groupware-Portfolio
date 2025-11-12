a7d6ffbe9aa4c681eb3016c5f011aec64162f461
Robin-lee <cmin19991105@gmail.com>
2025-11-12 14:35:00 +0900

    AttendacnePage 카드 비율 조정

--- src/features/attendance/pages/AttendancePage.jsx
+++ src/features/attendance/pages/AttendancePage.jsx
@@ -1,40 +1,43 @@
 import { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import AttendanceBasicCard from '../components/chart-data/AttendanceBasicCard';
-import AttendanceSummaryCard from '../components/AttendanceSummaryCard';
+import AttendanceScheduleCard from '../components/AttendanceScheduleCard';
 import AttendanceWeekViewCard from '../components/AttendanceWeekViewCard';
-import { Grid } from '@mui/material';
-
-// ==============================|| DEFAULT DASHBOARD ||============================== //
+import AttendanceSummaryCard from '../components/AttendanceSummaryCard';
 
 export default function Dashboard() {
   const [isLoading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setLoading(false);
-  }, []);
+  useEffect(() => setLoading(false), []);
 
   return (
-    <Box
-      sx={{
-        p: 2,
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 2,
-        flexGrow: 1,
-        overflow: 'visible'
-      }}
-    >
-      {/* 근태 카드 */}
-      <Box sx={{ flexShrink: 0 }}>
-        <AttendanceBasicCard isLoading={isLoading} />
+    <Box sx={{ p: 2, display: 'flex', flexDirection: 'column', gap: 2 }}>
+      {/*  근태 카드 + 일정 카드 */}
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          gap: 2,
+          alignItems: 'stretch'
+        }}
+      >
+        {/* 왼쪽 근태 카드 */}
+        <Box sx={{ flex: 7 }}>
+          <AttendanceBasicCard isLoading={isLoading} />
+        </Box>
+
+        {/* 오른쪽 일정 카드 */}
+        <Box sx={{ flex: 3 }}>
+          <AttendanceScheduleCard />
+        </Box>
       </Box>
-      {/* 주간 근무 카드 */}
-      <Box item xs={12}>
+
+      {/*  주간 근무 현황 */}
+      <Box>
         <AttendanceWeekViewCard />
       </Box>
-      {/* 근무 요약 카드 */}
-      <Box sx={{ flexShrink: 0 }}>
+
+      {/* 전체 근무 내역 */}
+      <Box>
         <AttendanceSummaryCard />
       </Box>
     </Box>
