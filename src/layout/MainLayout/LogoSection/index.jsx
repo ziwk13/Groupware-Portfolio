91bbcba28a5be99f0392f8540e834f371d926ff3
kht <gudxor1541@naver.com>
2025-11-20 15:27:27 +0900

    기존 Logo를 삭제하고 새로운 logo.png로 대체

--- src/layout/MainLayout/LogoSection/index.jsx
+++ src/layout/MainLayout/LogoSection/index.jsx
@@ -2,17 +2,30 @@ import { Link as RouterLink } from 'react-router-dom';
 
 // material-ui
 import Link from '@mui/material/Link';
-
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 // project imports
 import { DASHBOARD_PATH } from 'config';
-import Logo from 'ui-component/Logo';
+
+// assets
+import logo from 'assets/images/logo.png';
 
 // ==============================|| MAIN LOGO ||============================== //
 
 export default function LogoSection() {
   return (
-    <Link component={RouterLink} to={DASHBOARD_PATH} aria-label="theme-logo">
-      <Logo />
+    <Link
+      component={RouterLink}
+      to={DASHBOARD_PATH}
+      aria-label="theme-logo"
+      underline="none"
+    >
+      <Stack direction="row" spacing={1} alignItems="center">
+        <img width="32" height="32" src={logo} alt="Logo" />
+        <Typography variant="h3" sx={{ color: 'text.primary', fontWeight: 1000, pl:'7px'}}>
+          STARTUP
+        </Typography>
+      </Stack>
     </Link>
   );
 }
